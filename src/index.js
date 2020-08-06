92eb63c4c4c5d929b36199e46a2239692d2eb0c5
D Otasek <dotasek.dev@gmail.com>
2020-08-06 14:47:53 -0400

    Refactor before adding more login components

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ import styles from './styles.module.css'
 export { default as OpenInCytoscapeButton } from './OpenInCytoscapeButton';
 export * from './OpenInCytoscapeButton';
 
-export { default as NDExSignInButton } from './NDExSignInButton';
-export * from './NDExSignInButton';
+export { default as NDExSignInButton } from './NDExSignIn';
+export * from './NDExSignIn';
